a52b35bc4209d8779207a05d54629ff42838d75a
Mohamed Ahmed Galal <mohamedahmedgalal246@gmail.com>
2025-01-29 18:14:43 +0200

    fifth

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
+#include<string>
 
-int Mul(int i, int j)
-{
-    return i*j;
-}
+class student{
+public:
+    int id =0 ;
+    string name;
+    void setname(string name)
+    {
+        this->name = name;
+    }
+    void setid(int id)
+    {
+        this->id = id;
+    }
+    void print()
+    {
+        cout<<this->id <<endl;
+        cout<<this->name<<endl;
+        cout<<"====================\n";
+    }
+};
+
 int main()
 {
-cout<<Mul(2,5)<<endl;
-cout<<Mul(2,6)<<endl;
-cout<<"Hello World"<<endl;
+
+    student s1;
+    s1.setid(111);
+    s1.setname("Mohamed");
+    s1.print();
 }
